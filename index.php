61130c8b3669f356840286ea33052d1a5fa6de41
DevApps <contato@devapps.com.br>
2021-08-18 21:56:46 -0300

    Initial struct.

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <title>BlueTv Planos</title>
+</head>
+<body>
+    <!-- Wrapper -->
+    <div class="da-wrapper">
+        <header class="da-header">
+            <div class="da-navbar">
+                <div class="container">
+                    <div class="row align-item-center">
+                        <div class="col-lg-12">
+                            <nav class="navbar navbar-expand-lg">
+                                <a href="#da_home" class="navbar-brand">
+                                    <img src="" alt="BlueTv Planos">
+                                </a>
+                                <button class="navbar-toggler" type="button" 
+                                    data-bs-toggle="collapse" 
+                                    data-bs-target="#da_main_menu" 
+                                    aria-controls="da_main_menu" 
+                                    aria-expanded="false"
+                                    aria-label="Toggle navigation">
+                                    <div class="toggler-icon"></div>
+                                    <div class="toggler-icon"></div>
+                                    <div class="toggler-icon"></div>
+                                </button>
+                                <div class="collapse navbar-collapse" id="da_main_menu">
+                                    <ul class="navbar-nav ms-auto" id="da-nav">
+                                        <li class="nav-item">
+                                            <a href="#da_home" class="da-page-scroll">Início</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="#da_plans" class="da-page-scroll">Planos</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="#da_send" class="da-page-scroll">Envio</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="#da_download" class="da-page-scroll">Downloads</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="#da_faq" class="da-page-scroll">FAQ</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="navbar-btn d-none d-sm-inline-block">
+                                    <a href="#downloads" class="da-page-scroll da-btn-3" data-scroll-nav="0">
+                                        Teste Grátis Por 7 Dias
+                                    </a>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <section id="da_home" class="da-hero">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-5">
+                        <div class="da-align-center">
+                            <div class="da-hero-content">
+                                <h1>
+                                    Todos Os Canais
+                                    <span>
+                                        <br>
+                                        No Melhor App Tv
+                                    </span>
+                                </h1>
+                                <h2>
+                                    <i class="lni lni-star-filled"></i>
+                                    Promoção
+                                    <i class="lni lni-star-filled"></i>
+                                </h2>
+                                <br>
+                                <br>
+                                <p>
+                                    <span>R$ 19,90</span>
+                                    <br>
+                                    BlueTV Plano Mensal
+                                </p>
+                                <a href="#" class="da-main-btn da-uppercase">
+                                    Comprar
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-7">
+                        <div class="da-hero-image">
+                            <img src="" alt="" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <!-- End Wrapper -->
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+</body>
+</html>
